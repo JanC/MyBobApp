3fef6ef62647e952848fb6ae942e12670cd1a605
Jan Chaloupecky <jan.chaloupecky@n26.com>
2019-08-19 11:09:37 +0200

    first commit

---
+++ MyBobApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBobApp
+//
+//  Created by Jan Chaloupecky on 19.08.19.
+//  Copyright © 2019 AeroNavMap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
